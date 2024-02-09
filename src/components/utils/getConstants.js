a16a0349ed981f30ae29063c3e46587b2155ba5b
ScottGrizzell <scott.grizzell@oit.edu>
2024-02-08 17:09:36 -0800

    Refactored to be compliant with discordcert-ui instead of identicert

--- src/components/utils/getConstants.js
+++ src/components/utils/getConstants.js
@@ -1,19 +1,15 @@
 const getConstants = () => {
-  let certifierUrl, certifierPublicKey, confederacyUrl, personaInquiryTemplateID, personaInquiryEnvId
-  const certificateType = 'z40BOInXkI8m7f/wBrv4MJ09bZfzZbTj2fJqCtONqCY='
+  let certifierUrl, certifierPublicKey, confederacyUrl
+  const certificateType = '2TgqRC35B1zehGmB21xveZNc7i5iqHc0uxMb+1NMPW4=' // correct
 
   if (window.location.host.startsWith('localhost') || (window.location.host.startsWith('staging'))) {
-    certifierUrl = 'https://staging-identicert-backend.babbage.systems'
-    certifierPublicKey = '036dc48522aba1705afbb43df3c04dbd1da373b6154341a875bceaa2a3e7f21528'
+    certifierUrl = 'https://staging-discordcert-backend.babbage.systems'
+    certifierPublicKey = '02cf6cdf466951d8dfc9e7c9367511d0007ed6fba35ed42d425cc412fd6cfd4a17' // correct
     confederacyUrl = 'https://staging-confederacy.babbage.systems'
-    personaInquiryTemplateID = 'itmpl_UAr6mTrGB2xUGDWwAMr1Kjy8'
-    personaInquiryEnvId = 'env_Anm1qGVdq3t3cwgWgwmp7ro6'
   } else {
-    certifierUrl = 'https://identicert-backend.babbage.systems'
-    certifierPublicKey = '0295bf1c7842d14babf60daf2c733956c331f9dcb2c79e41f85fd1dda6a3fa4549'
+    certifierUrl = 'https://discordcert-backend.babbage.systems'
+    certifierPublicKey = '03285263f06139b66fb27f51cf8a92e9dd007c4c4b83876ad6c3e7028db450a4c2' // correct
     confederacyUrl = 'https://confederacy.babbage.systems'
-    personaInquiryTemplateID = 'itmpl_UAr6mTrGB2xUGDWwAMr1Kjy8'
-    personaInquiryEnvId = 'env_XTt9RSHyrcAV4NGpzuC8YWyk'
   }
 
   return {
@@ -21,8 +17,6 @@ const getConstants = () => {
     certifierPublicKey,
     certificateType,
     confederacyUrl,
-    personaInquiryTemplateID,
-    personaInquiryEnvId
   }
 }
 export default getConstants
--- src/pages/AuthRedirect.jsx
+++ src/pages/AuthRedirect.jsx
@@ -1,17 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import React from 'react'
+import getConstants from '../utils/getConstants'
+import './ipersona.css'
+import { Dialog } from '@mui/material'
+import Persona from 'persona'
+import { Signia } from 'babbage-signia'
+import { toast } from 'react-toastify'
+
+
+
 
 const AuthRedirect = () =>{
+  const [progressStatus, setProgressStatus] = useState("")
+  const constants = getConstants()
+  const signia = new Signia()
+  signia.config.confederacyHost = constants.confederacyUrl
+
     useEffect(() =>{
         const params = new URLSearchParams(window.location.search);
         const code = params.get('code');
-
-        console.log(code);
+        (async ()=>{
+          await signia.publiclyRevealAttributes({}, constants.certifierUrl, constants.certifierPublicKey, constants.certificateType,
+            true, {accessCode: code}, async (message)=>{
+              setProgressStatus(message)
+            })
+        })()
+        
+        
 
     }, []);
-   // return window.location.href = "https://youtube.com";
+
    return (
+    <div>
     <div>Processing...</div>
-    
+    <p>{progressStatus}</p>
+    </div>
   );
 }
 
