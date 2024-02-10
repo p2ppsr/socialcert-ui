fb386905619734db7579472698342a11f4149de3
ScottGrizzell <scott.grizzell@oit.edu>
2024-02-10 12:55:47 -0800

    Added proper redirect logic to Auth.jsx

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -2,9 +2,16 @@ import React, { useState } from 'react'
 
 
     const Auth = () =>{
-    
-        // TODO: This link will need to be remade for not a localhost redirect
-    return window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1202716017055375421&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8088%2FAuthRedirect&scope=identify"; 
+    const hostname = window.location.hostname;
+
+    if(hostname.includes("staging")){
+        return window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1202716017055375421&response_type=code&redirect_uri=https%3A%2F%2Fstaging-discordcert.babbage.systems%2Fauthredirect&scope=identify"
+    }
+    else{
+       return window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1202716017055375421&response_type=code&redirect_uri=https%3A%2F%2Fdiscordcert.babbage.systems%2Fauthredirect&scope=identify"
+    }
+
+
     }
 
 
