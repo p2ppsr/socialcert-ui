e4b96cd9596964c24716c0f5b1cb8440d32aa840
ScottGrizzell <scott.grizzell@oit.edu>
2024-02-15 13:28:14 -0800

    Added redirect to localhost for local testing

--- src/components/utils/getConstants.js
+++ src/components/utils/getConstants.js
@@ -2,11 +2,17 @@ const getConstants = () => {
   let certifierUrl, certifierPublicKey, confederacyUrl
   const certificateType = '2TgqRC35B1zehGmB21xveZNc7i5iqHc0uxMb+1NMPW4=' // correct
 
-  if (window.location.host.startsWith('localhost') || (window.location.host.startsWith('staging'))) {
+  if ( (window.location.host.startsWith('staging'))) {
     certifierUrl = 'https://staging-discordcert-backend.babbage.systems'
     certifierPublicKey = '02cf6cdf466951d8dfc9e7c9367511d0007ed6fba35ed42d425cc412fd6cfd4a17' // correct
     confederacyUrl = 'https://staging-confederacy.babbage.systems'
-  } else {
+  }
+  
+  else if(window.location.host.startsWith('localhost')){
+    certifierUrl = 'http://localhost:3002'
+    certifierPublicKey = '02cf6cdf466951d8dfc9e7c9367511d0007ed6fba35ed42d425cc412fd6cfd4a17' // correct
+    confederacyUrl = 'https://staging-confederacy.babbage.systems'
+  }else {
     certifierUrl = 'https://discordcert-backend.babbage.systems'
     certifierPublicKey = '03285263f06139b66fb27f51cf8a92e9dd007c4c4b83876ad6c3e7028db450a4c2' // correct
     confederacyUrl = 'https://confederacy.babbage.systems'
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -5,6 +5,11 @@
     if(hostname.includes("staging")){
         return window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1202716017055375421&response_type=code&redirect_uri=https%3A%2F%2Fstaging-discordcert.babbage.systems%2Fauthredirect&scope=identify"
     }
+
+    else if(hostname.includes("localhost")){
+        return window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1202716017055375421&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8088%2FAuthRedirect&scope=identify"
+    }
+
     else{
        return window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1202716017055375421&response_type=code&redirect_uri=https%3A%2F%2Fdiscordcert.babbage.systems%2Fauthredirect&scope=identify"
     }
