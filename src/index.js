5f82e7793e374f01d4a5227e475e3e0338fb5b55
Brayden Langley <brayden167@gmail.com>
2024-03-07 13:23:55 -0800

    Remove prompt for testing

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import BabbagePrompt from '@babbage/react-prompt'
 
 ReactDOM.render(
-  <BabbagePrompt
-    customPrompt
-    appName='DiscordCert'
-    author='Project Babbage'
-    authorUrl='https://projectbabbage.com'
-    description='Babbage Certified Identity'
-    appIcon='/images/identiCertIcon.png'
-    appImages={[
-      '/images/startPage.png'
-    ]}
-    supportedMetaNet='testnet'
-  >
-    <App />
-  </BabbagePrompt>,
+  <App />,
   document.getElementById('root')
 )
