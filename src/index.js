1bc5dfda987f96b6ef3e500a4eeda358167cd2b6
Brayden Langley <brayden167@gmail.com>
2024-03-07 13:30:11 -0800

    Update deps

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import BabbagePrompt from '@babbage/react-prompt'
 
 ReactDOM.render(
-  <App />,
+  <BabbagePrompt
+    customPrompt
+    appName='DiscordCert'
+    author='Project Babbage'
+    authorUrl='https://projectbabbage.com'
+    description='Babbage Certified Identity'
+    appIcon='/images/identiCertIcon.png'
+    appImages={[
+      '/images/startPage.png'
+    ]}
+    supportedMetaNet='testnet'
+  >
+    <App />
+  </BabbagePrompt>,
   document.getElementById('root')
 )
