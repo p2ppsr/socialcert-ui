e59f11ffbe7dee8a2e2e89dd64f0a37393812952
Brayden Langley <brayden167@gmail.com>
2024-05-17 15:02:13 -0700

    Updated dep

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import BabbagePrompt from "@babbage/react-prompt"
+import ReactDOM from 'react-dom'
+import App from './App'
 
 ReactDOM.render(
   <App />,
-  document.getElementById("root")
+  document.getElementById('root')
 )
-
-{
-  /* <BabbagePrompt
-// customPrompt
-appName='SocialCert'
-author='Project Babbage'
-authorUrl='https://projectbabbage.com'
-description='Babbage Certified Identity'
-appIcon={{
-  mainnet: '/images/socialCertIcon.png',
-  testnet: '/images/socialCertIcon.png'
-}}
-appImages={[
-  {
-    mainnet: '/images/socialCertIcon.png',
-    testnet: '/images/socialCertIcon.png'
-  }
-]}
-supportedMetaNet='testnet'
->
-</BabbagePrompt>, */
-}
